cc58a32bb83d95bada23a052f44561775ab87781
gergely.kiss <gergely@hyperplane.hu>
2023-06-28 14:36:29 +0200

    UI: Responsivity added for FeaturedPosts component

--- components/FeaturedPosts.tsx
+++ components/FeaturedPosts.tsx
@@ -6,13 +6,13 @@ type Props = { featuredPosts: IFeaturedPost[] };
 
 const FeaturedPosts: FC<Props> = ({ featuredPosts }) => {
   return (
-    <div className="grid h-[500px] grid-cols-12 gap-5">
+    <div className="grid h-[1200px] grid-cols-12 gap-5 md:h-[500px]">
       <FeaturedPostItem
         featuredPost={featuredPosts[0]}
-        className="col-span-7"
+        className="col-span-12 md:col-span-7"
         variant="main"
       />
-      <div className="col-span-5 grid grid-cols-1 gap-5">
+      <div className="col-span-12 grid grid-cols-1 gap-5 md:col-span-5">
         {featuredPosts.slice(1).map((post) => (
           <FeaturedPostItem
             key={post.slug}
