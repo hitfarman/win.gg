57a6bcf7c60950cd6382d9e765380642f2e9ce12
gergely.kiss <gergely@hyperplane.hu>
2023-07-05 12:42:27 +0200

    Feature: Added privacy policy page

--- pages/terms-of-service/index.tsx
+++ pages/terms-of-service/index.tsx
@@ -39,20 +39,16 @@ export const getStaticProps: GetStaticProps = async () => {
     return { notFound: true };
   }
 
+  pageInfo.content = pageInfo.content
+    .replace(
+      "[et_pb_section fb_built=&#8221;1&#8243; admin_label=&#8221;TERMS OF SERVICE\r&#8221; _builder_version=&#8221;4.19.4&#8243; hover_enabled=&#8221;0&#8243; global_colors_info=&#8221;{}&#8221; background_color=&#8221;RGBA(255,255,255,0)&#8221; sticky_enabled=&#8221;0&#8243;][et_pb_row admin_label=&#8221;TERMS OF SERVICE&#8221; _builder_version=&#8221;4.19.4&#8243; background_size=&#8221;initial&#8221; background_position=&#8221;top_left&#8221; background_repeat=&#8221;repeat&#8221; hover_enabled=&#8221;0&#8243; global_colors_info=&#8221;{}&#8221; background_color=&#8221;RGBA(255,255,255,0)&#8221; sticky_enabled=&#8221;0&#8243;][et_pb_column type=&#8221;4_4&#8243; _builder_version=&#8221;4.16&#8243; custom_padding=&#8221;|||&#8221; global_colors_info=&#8221;{}&#8221; custom_padding__hover=&#8221;|||&#8221;][et_pb_post_title _builder_version=&#8221;4.19.4&#8243; _module_preset=&#8221;default&#8221; meta=&#8221;off&#8221; featured_image=&#8221;off&#8221; hover_enabled=&#8221;0&#8243; sticky_enabled=&#8221;0&#8243; admin_label=&#8221;TERMS OF SERVICE &#8211; Title&#8221;][/et_pb_post_title][et_pb_text admin_label=&#8221;TERMS OF SERVICE &#8211; Text&#8221; _builder_version=&#8221;4.16&#8243; background_size=&#8221;initial&#8221; background_position=&#8221;top_left&#8221; background_repeat=&#8221;repeat&#8221; global_colors_info=&#8221;{}&#8221;]",
+      ""
+    )
+    .replace("[/et_pb_text][/et_pb_column][/et_pb_row][/et_pb_section]", "");
+
   return {
     props: {
-      pageInfo: {
-        ...pageInfo,
-        content: pageInfo.content
-          .replace(
-            "[et_pb_section fb_built=&#8221;1&#8243; admin_label=&#8221;TERMS OF SERVICE\r&#8221; _builder_version=&#8221;4.19.4&#8243; hover_enabled=&#8221;0&#8243; global_colors_info=&#8221;{}&#8221; background_color=&#8221;RGBA(255,255,255,0)&#8221; sticky_enabled=&#8221;0&#8243;][et_pb_row admin_label=&#8221;TERMS OF SERVICE&#8221; _builder_version=&#8221;4.19.4&#8243; background_size=&#8221;initial&#8221; background_position=&#8221;top_left&#8221; background_repeat=&#8221;repeat&#8221; hover_enabled=&#8221;0&#8243; global_colors_info=&#8221;{}&#8221; background_color=&#8221;RGBA(255,255,255,0)&#8221; sticky_enabled=&#8221;0&#8243;][et_pb_column type=&#8221;4_4&#8243; _builder_version=&#8221;4.16&#8243; custom_padding=&#8221;|||&#8221; global_colors_info=&#8221;{}&#8221; custom_padding__hover=&#8221;|||&#8221;][et_pb_post_title _builder_version=&#8221;4.19.4&#8243; _module_preset=&#8221;default&#8221; meta=&#8221;off&#8221; featured_image=&#8221;off&#8221; hover_enabled=&#8221;0&#8243; sticky_enabled=&#8221;0&#8243; admin_label=&#8221;TERMS OF SERVICE &#8211; Title&#8221;][/et_pb_post_title][et_pb_text admin_label=&#8221;TERMS OF SERVICE &#8211; Text&#8221; _builder_version=&#8221;4.16&#8243; background_size=&#8221;initial&#8221; background_position=&#8221;top_left&#8221; background_repeat=&#8221;repeat&#8221; global_colors_info=&#8221;{}&#8221;]",
-            ""
-          )
-          .replace(
-            "[/et_pb_text][/et_pb_column][/et_pb_row][/et_pb_section]",
-            ""
-          )
-      }
+      pageInfo
     },
     revalidate: 60 * 15
   };
