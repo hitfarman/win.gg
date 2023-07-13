d9c4efbbf4d51b64786cbf9f36ed77671bfc44d6
gergely.kiss <gergely@hyperplane.hu>
2023-07-13 15:28:40 +0200

    Fix: Updated error boundary content

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -26,24 +26,20 @@ class ErrorBoundary extends Component<Props, State> {
   public render() {
     if (this.state.hasError) {
       return (
-        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-          <div className="text-center">
-            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-              Hiba történt!
-            </h1>
-            <p className="mt-6 text-base leading-7 text-gray-600">
-              Sajnos hibába ütköztünk az oldal betöltése során.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <Link
-                href="/"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Vissza a főoldalra
-              </Link>
-            </div>
+        <div className="flex h-screen items-center justify-center">
+          <div className="flex flex-col items-center gap-5 pb-5 text-center">
+            <h1 className="font-header text-4xl font-semibold">Opps!</h1>
+            <h2 className="font-header text-2xl font-semibold">
+              Something went wrong.
+            </h2>
+            <h3 className="font-header text-base text-win-slate">
+              We are sorry for the inconvenience.
+            </h3>
+            <Link href="/" className="win-primary-button">
+              Back to home
+            </Link>
           </div>
-        </main>
+        </div>
       );
     }
 
