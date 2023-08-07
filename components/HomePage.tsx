18b796df6d63a57aed50f2b704a5eb045f7bd3e4
gergely.kiss <gergely@hyperplane.hu>
2023-08-07 16:47:55 +0200

    Fix: Removed subnavigation on client side from homepage

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,5 +1,5 @@
 import { IHomePageProps } from "@/interfaces/pageProps";
-import React, { FC, useEffect, useState } from "react";
+import React, { FC } from "react";
 import FeaturedPosts from "@/components/FeaturedPosts";
 import FeaturedVideos from "@/components/FeaturedVideos";
 import PostList from "@/components/PostList";
@@ -8,15 +8,6 @@ import FeaturedReviews from "@/components/FeaturedReviews";
 import FeaturedVideosSecondary from "@/components/FeaturedVideosSecondary";
 import parse from "html-react-parser";
 import { replaceImage } from "@/utils/replaceImage";
-import { POSTS_PER_PAGE } from "@/constants/posts";
-import { useQuery } from "@apollo/client";
-import {
-  IPaginatedPostsResponse,
-  IPostQueryVariables
-} from "@/interfaces/posts";
-import { GET_PAGINATED_POSTS } from "@/apollo/posts";
-import { useRouter } from "next/router";
-import { calculatePaginationOffset } from "@/utils/calculatePaginationOffset";
 import Head from "next/head";
 
 const HomePage: FC<IHomePageProps> = ({
@@ -28,72 +19,6 @@ const HomePage: FC<IHomePageProps> = ({
   paginatedPosts,
   pageSeo
 }) => {
-  // State
-  const router = useRouter();
-  const [postResponseToShow, setPostsResponseToShow] =
-    useState<IPaginatedPostsResponse | null>(paginatedPosts);
-  const [skipUseQuery, setSkipUseQuery] = useState<boolean>(true);
-  const [queryVars, setQueryVars] = useState<IPostQueryVariables | null>(null);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-
-  const { loading, data: posts } = useQuery<IPaginatedPostsResponse>(
-    GET_PAGINATED_POSTS,
-    {
-      variables: queryVars || {},
-      ssr: false,
-      skip: skipUseQuery
-    }
-  );
-
-  useEffect(() => {
-    const filterParam = router.query.filter;
-    if (filterParam) {
-      setSkipUseQuery(false);
-      setQueryVars({
-        categoryName: String(filterParam),
-        offset: 0,
-        size: POSTS_PER_PAGE
-      });
-    } else {
-      setSkipUseQuery(true);
-      setCurrentPage(1);
-      setPostsResponseToShow(paginatedPosts);
-    }
-  }, [router.query, paginatedPosts]);
-
-  useEffect(() => {
-    if (posts && !skipUseQuery) {
-      setPostsResponseToShow(posts);
-    }
-  }, [posts, skipUseQuery]);
-
-  const changePage = (type: "next" | "prev" | "direct", to?: number) => {
-    let updatedNumber = 0;
-    if (type === "next") {
-      setCurrentPage((prev) => {
-        updatedNumber = prev + 1;
-        return updatedNumber;
-      });
-    }
-    if (type === "prev") {
-      setCurrentPage((prev) => {
-        updatedNumber = prev - 1;
-        return updatedNumber;
-      });
-    }
-    if (type === "direct" && to) {
-      setCurrentPage(() => {
-        updatedNumber = to;
-        return updatedNumber;
-      });
-    }
-
-    setQueryVars((prev) => ({
-      ...prev,
-      offset: calculatePaginationOffset(updatedNumber)
-    }));
-  };
-
   return (
     <>
       <Head>{parse(pageSeo)}</Head>
@@ -101,17 +26,7 @@ const HomePage: FC<IHomePageProps> = ({
       <FeaturedVideos featuredVideos={featuredVideos.slice(0, 3)} />
       <div className="flex flex-col gap-10 py-10 md:flex-row">
         <div className="flex-1">
-          <PostList
-            subNavigationProps={{
-              loading,
-              hasSubNavigation: true,
-              isActive: !!router.query.filter,
-              changePage,
-              currentPage
-            }}
-            paginatedPosts={postResponseToShow}
-            title="Latest news"
-          />
+          <PostList paginatedPosts={paginatedPosts} title="Latest news" />
         </div>
         <div className="md:w-4/12">
           <FeaturedTags tags={homeTags} />
--- components/PostList.tsx
+++ components/PostList.tsx
@@ -3,79 +3,53 @@ import React, { FC } from "react";
 import PostCard from "@/components/PostCard";
 import { useRouter } from "next/router";
 import PaginationNumbers from "@/components/PaginationNumbers";
-import SubNavigation from "@/components/SubNavigation";
-import { POSTS_PER_PAGE } from "@/constants/posts";
-import PostSkeleton from "./PostSkeleton";
 
 type Props = {
   paginatedPosts: IPaginatedPostsResponse | null;
   title?: string;
-  subNavigationProps?: {
-    hasSubNavigation: boolean;
-    isActive: boolean;
-    loading: boolean;
-    changePage: (type: "next" | "prev" | "direct", to?: number) => void;
-    currentPage: number;
-  };
   isReviewPage?: boolean;
 };
 
-const PostList: FC<Props> = ({
-  paginatedPosts,
-  title,
-  subNavigationProps,
-  isReviewPage
-}) => {
+const PostList: FC<Props> = ({ paginatedPosts, title, isReviewPage }) => {
   // Constant
-  const postSkeletons = new Array(POSTS_PER_PAGE).fill(0);
   const { asPath, push } = useRouter();
   const pageParamIsInUrl = /\/page\/\d+/.test(asPath);
   const pageNumber = pageParamIsInUrl ? parseInt(asPath.split("/page/")[1]) : 1;
 
   // Methods
   const changePage = (type: "next" | "prev" | "direct", to?: number) => {
-    if (subNavigationProps?.isActive) {
-      return subNavigationProps.changePage(type, to);
-    }
-
-    const cleanPath = `${asPath.replace(/\?filter=.*/, "")}`;
-
     if (type === "next") {
       if (!pageParamIsInUrl) {
         return push(
-          `${cleanPath}${
-            cleanPath[cleanPath.length - 1] === "/" ? "" : "/"
-          }page/2`
+          `${asPath}${asPath[asPath.length - 1] === "/" ? "" : "/"}page/2`
         );
       }
 
       return push(
-        cleanPath.replace(`/page/${pageNumber}`, `/page/${pageNumber + 1}`)
+        asPath.replace(`/page/${pageNumber}`, `/page/${pageNumber + 1}`)
       );
     }
     if (type === "prev") {
       if (pageNumber === 2) {
-        return push(cleanPath.replace(`/page/${pageNumber}`, "/"));
+        return push(asPath.replace(`/page/${pageNumber}`, "/"));
       }
 
       return push(
-        cleanPath.replace(`/page/${pageNumber}`, `/page/${pageNumber - 1}`)
+        asPath.replace(`/page/${pageNumber}`, `/page/${pageNumber - 1}`)
       );
     }
     if (type === "direct") {
       if (to === 1) {
-        return push(cleanPath.replace(`/page/${pageNumber}`, "/"));
+        return push(asPath.replace(`/page/${pageNumber}`, "/"));
       }
 
       if (!pageParamIsInUrl) {
         return push(
-          `${cleanPath}${
-            cleanPath[cleanPath.length - 1] === "/" ? "" : "/"
-          }page/${to}`
+          `${asPath}${asPath[asPath.length - 1] === "/" ? "" : "/"}page/${to}`
         );
       }
 
-      return push(cleanPath.replace(`/page/${pageNumber}`, `/page/${to}`));
+      return push(asPath.replace(`/page/${pageNumber}`, `/page/${to}`));
     }
   };
 
@@ -86,33 +60,25 @@ const PostList: FC<Props> = ({
           {title}
         </h3>
       )}
-      {subNavigationProps?.hasSubNavigation && <SubNavigation />}
-      {subNavigationProps?.loading ? (
-        postSkeletons.map((_, i) => <PostSkeleton key={`${i}-post-skeleton`} />)
-      ) : (
-        <div>
-          {paginatedPosts ? (
-            paginatedPosts?.posts.edges.map((post) => (
-              <PostCard
-                key={post.node.id}
-                post={post.node}
-                isReviewPage={isReviewPage}
-              />
-            ))
-          ) : (
-            <p>Sorry, there are currently no posts available!</p>
-          )}
-        </div>
-      )}
+
+      <div>
+        {paginatedPosts ? (
+          paginatedPosts?.posts.edges.map((post) => (
+            <PostCard
+              key={post.node.id}
+              post={post.node}
+              isReviewPage={isReviewPage}
+            />
+          ))
+        ) : (
+          <p>Sorry, there are currently no posts available!</p>
+        )}
+      </div>
 
       <PaginationNumbers
         className="mt-5 flex flex-wrap justify-center gap-2 text-xs sm:hidden"
         changePage={changePage}
-        pageNumber={
-          subNavigationProps?.isActive
-            ? subNavigationProps.currentPage
-            : pageNumber
-        }
+        pageNumber={pageNumber}
         siblingCount={1}
         total={
           paginatedPosts?.posts.pageInfo.offsetPagination.total || pageNumber
@@ -135,11 +101,7 @@ const PostList: FC<Props> = ({
         <PaginationNumbers
           className="hidden gap-2 sm:flex"
           changePage={changePage}
-          pageNumber={
-            subNavigationProps?.isActive
-              ? subNavigationProps.currentPage
-              : pageNumber
-          }
+          pageNumber={pageNumber}
           siblingCount={1}
           total={
             paginatedPosts?.posts.pageInfo.offsetPagination.total || pageNumber
