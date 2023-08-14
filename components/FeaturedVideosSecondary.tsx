0fb01f6d3f645fd0645e1958d842d39f10d2bed7
András <andras@hyerplane.hu>
2023-08-14 20:02:27 +0200

    Feat: Remove frontpage videos from seconday video list in the sidebar

--- components/FeaturedVideosSecondary.tsx
+++ components/FeaturedVideosSecondary.tsx
@@ -11,15 +11,6 @@ const FeaturedVideosSecondary: FC<Props> = ({ featuredVideos }) => {
   return (
     <div className="grid grid-cols-1 gap-5">
       <InsertedAd id="direct-ad-above-video" className="min-h-[250px]" />
-      {featuredVideos.slice(0, 2).map((video) => (
-        <LazyYoutubeVideo
-          key={video.url}
-          url={video.url}
-          height={250}
-          width={350}
-          sizesForNextImg="(max-width: 768px) 100vw, 33vw"
-        />
-      ))}
       {featuredVideos.slice(2).map((video) => (
         <LazyYoutubeVideo
           key={video.url}
