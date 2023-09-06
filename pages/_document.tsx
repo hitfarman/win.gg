f5c049c3c64b54752c277e65e482fc80aef0a8e5
András Vidosits <vidosits.andras@gmail.com>
2023-09-06 10:33:47 +0200

    Temproarily turn off ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,50 +4,10 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-W3YZ88PL2C');
-            `
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(t){function s(s,a){t.revamp.cmd.push([s,a])}t.revamp=t.revamp||{},t.revamp.cmd=[],t.revamp.displaySlots=t.revamp.displaySlots||function(){s("displayDeferredSlots",arguments)},t.revamp.destroySlots=t.revamp.destroySlots||function(){s("destroyGptSlots",arguments)},t.revamp.displayCustomInterstitial=t.revamp.displayCustomInterstitial||function(){s("displayCustomInterstitial",arguments)}}(window);;
-            `
-          }}
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
-        <Script
-          strategy="beforeInteractive"
-          src="https://spn-v1.revampcdn.com/prebid/win-gg/prebid-client.js"
-        />
-        <Script
-          strategy="lazyOnload"
-          type="module"
-          src="https://spn-v1.revampcdn.com/publishers/win-gg.js?modern=1"
-        />
-        <Script
-          strategy="lazyOnload"
-          noModule
-          src="https://spn-v1.revampcdn.com/publishers/win-gg.js"
-        />
-        <Script
-          strategy="lazyOnload"
-          src="https://www.googletagmanager.com/gtag/js?id=G-W3YZ88PL2C"
-        />
-        <Script
-          strategy="lazyOnload"
-          src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
-        />
       </body>
     </Html>
   );
