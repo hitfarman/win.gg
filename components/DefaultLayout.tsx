39130ae8a1586691c2ccb1d2b24ec2f077876e23
András Vidosits <vidosits.andras@gmail.com>
2024-05-24 18:55:40 +0200

    Feature: Add top banner and sidebar ads

--- components/DefaultLayout.tsx
+++ components/DefaultLayout.tsx
@@ -1,6 +1,9 @@
 import { FC, ReactNode } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Link from "next/link";
+import Image from "next/image";
+import bannerAd from "@/assets/img/Banners-728x90.png";
 
 type Props = {
   children: ReactNode;
@@ -10,7 +13,17 @@ const DefaultLayout: FC<Props> = ({ children }) => {
   return (
     <div className="mx-auto flex min-h-screen max-w-7xl flex-col p-4 pb-0 sm:px-6 lg:px-8">
       <Header />
-      <main className="flex-[1_1_0] pt-24">{children}</main>
+      <Link href="https://stake.us/?c=L7mkCg40">
+        <div className="flex flex-row justify-center">
+          <Image
+            src={bannerAd}
+            alt="Stake"
+            rel="nofollow"
+            className="full-bleed-black mt-16 hidden bg-black py-2 md:flex md:min-h-[110px]"
+          />
+        </div>
+      </Link>
+      <main className="flex-[1_1_0] pt-24 md:pt-12">{children}</main>
       <Footer />
     </div>
   );
--- components/FeaturedSidebar.tsx
+++ components/FeaturedSidebar.tsx
@@ -5,6 +5,9 @@ import React, { FC } from "react";
 import FeaturedTags from "@/components/FeaturedTags";
 import FeaturedReviews from "@/components/FeaturedReviews";
 import FeaturedVideosSecondary from "@/components/FeaturedVideosSecondary";
+import Link from "next/link";
+import Image from "next/image";
+import sidebarAd from "@/assets/img/Stake-Banners-336x280.jpg";
 
 type Props = {
   featuredTags: IFeaturedTag[];
@@ -21,6 +24,17 @@ const FeaturedSidebar: FC<Props> = ({
     <div>
       <FeaturedTags tags={featuredTags} />
       <FeaturedReviews reviews={featuredReviews} />
+      <Link
+        href="https://stake.us/?c=L7mkCg40"
+        className="flex flex-row justify-center"
+      >
+        <Image
+          src={sidebarAd}
+          alt="Stake"
+          rel="nofollow"
+          className="mb-5 object-cover"
+        />
+      </Link>
       <FeaturedVideosSecondary featuredVideos={featuredVideos} />
     </div>
   );
